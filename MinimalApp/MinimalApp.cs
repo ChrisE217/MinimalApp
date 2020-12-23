08ed38f26154cbcf183372afc239dd1434b5e029
Chris Ewart <chris.ewart1999@gmail.com>
2020-12-23 02:10:24 +0000

    test

--- MinimalApp/MinimalApp.cs
+++ MinimalApp/MinimalApp.cs
@@ -18,7 +18,7 @@ await Host.CreateDefaultBuilder(args)
                 {
                     using var db = new MinimalAppDbContext();
                     context.Response.ContentType = "text/html";
-                    var responseString = $"Add Item to In Memory DB</br><form action=\"/additem\" method=\"post\"><label for=\"name\">Name: </label></br><input type=\"text\" id=\"name\"/ name=\"name\" placeholder=\"Enter name\" maxlength=\"15\"><button type=\"submit\">Add</button></form></br><style>table, th, td{{border: 1px solid black}}</style><table><tr><th>Id</th><th>Name</th>";
+                    var responseString = $"Add Item to In Memory DB test deploy</br><form action=\"/additem\" method=\"post\"><label for=\"name\">Name: </label></br><input type=\"text\" id=\"name\"/ name=\"name\" placeholder=\"Enter name\" maxlength=\"15\"><button type=\"submit\">Add</button></form></br><style>table, th, td{{border: 1px solid black}}</style><table><tr><th>Id</th><th>Name</th>";
                     foreach (var i in await db.Items.ToListAsync()) responseString += $"<tr><td>{i.Id}</td><td>{i.Name}</td></tr>";
                     responseString += "</table>";
                     await context.Response.WriteAsync(responseString);
